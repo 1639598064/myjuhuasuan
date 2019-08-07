ddb2d99aaa5e64a3d56c3972b1daa37abc375c05
zml <18194249832@136.com>
2019-08-07 18:06:39 +0800

    8.7第一次上传项目

---
+++ js/goodslist.js
@@ -0,0 +1,98 @@
+//点击回到顶部
+
+function n(id) {
+    return document.getElementById(id);
+}
+window.onload = function () {
+    n("goTop").onclick = function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+}
+
+//点击隐藏显示
+$(function () {
+
+    $(window).scroll(function () {
+        // alert(1)
+        if ($(window).scrollTop() >= 250) {
+            $("#navRight").css({
+                transform: "scale(1)"
+            })
+        } else {
+            $("#navRight").css({
+                transform: "scale(0)"
+            })
+        }
+    });
+    $("#goTop").click(function () {
+
+        $("#navRight").css({
+            transform: "scale(0)",
+
+        })
+    });
+});
+
+//倒计时
+
+var starttime = new Date("2019/8/30");
+setInterval(function () {
+    var nowtime = new Date();
+    var time = starttime - nowtime;
+    var day = parseInt(time / 1000 / 60 / 60 / 24);
+    var hour = parseInt(time / 1000 / 60 / 60 % 24);
+    var minute = parseInt(time / 1000 / 60 % 60);
+    var seconds = parseInt(time / 1000 % 60);
+    $('.time').html(day + "天" + hour + "时" + minute + "分" + seconds + "秒");
+}, 800);
+
+
+//调取
+$(function () {
+    var bigDiv = $(".center");
+    var sDiv = "";
+    var sDiv2 = "";
+    $.ajaxSettings.async = false; //修改异步同步的方法
+    $.get("../goodsAndShoppingCart/getGoodsList.php", {}, function (data) {
+        var goods = data;
+
+        for (var i = 0; i < goods.length; i++) {
+
+            sDiv =
+                `
+            <div class="centerBox"><a href="#" class="box_content"><img src="${goods[i].goodsImg}" alt=""><div class="box_right"><h2>${goods[i].goodsName}</h2><ul><li>${goods[i].beiyong1}</li><li>${goods[i].beiyong2}</li><li>${goods[i].beiyong3}</li></ul><div class="box_time"><img src="img/time.png" alt="">仅剩：<b class="time"></b><strong>${goods[i].goodsCount}</strong><span>件已付款</span></div><div class="box_bottom"><i>￥</i><b>${goods[i].goodsPrice}</b><div class="bottom_money"><span>买贵赔</span><del>￥${goods[i].beiyong4}</del></div><span>退货赔运费</span></div><div class="bottom_right">马上抢</div></div></a></div>`;
+
+            sDiv2 += sDiv;
+        }
+        console.info(data)
+    }, "json")
+    bigDiv.append(sDiv2);
+})
+
+
+//点击划过后有边框
+$(document).ready(function () {
+    let $box_content = $(".box_content").length;
+    for (let i = 0; i < $box_content; i++) {
+
+        $(".box_content").eq(i).mouseover(function () {
+            $(".box_content").eq(i).css({
+                "border": "2px solid red"
+            });
+        });
+
+        $(".box_content").eq(i).mouseout(function () {
+            $(".box_content").eq(i).css({
+                "border": "2px solid transparent"
+            });
+        });
+
+    }
+});
+
+//点击抢购跳转到详情页面
+
+$("body").on("click", ".bottom_right", function () {
+    window.open("shoppingxqy.html");
+})
--- js/zhuceyz.js
+++ js/zhuceyz.js
@@ -1,77 +1,166 @@
-function $(id) {
-	return document.getElementById(id);
-}
-	window.onload=function () {
-
-//用户名判断
-	$("userId").onblur=function () {
-		var r = /^[a-zA-Z]\w{5,15}$/ig;   //6-16位数字、字母组合，且以字母开头
-		if(r.test($("userId").value)){
-			$("content1").style.display="none";
-		}else{
-			$("content1").style.display="block";
-			$("content1").value="！请输入正确用户名";
-		}
+//正则验证
+function check(str, type) {
+	switch (type) {
+		case "username":
+			var r = /^[a-zA-Z_]\w{5,14}$/;
+			break;
+			//数字，字母，下划线，6,16
+		case "userpass":
+			var r = /^\w{6,16}$/;
+			break;
+		case "email":
+			var r = /^[1-9a-zA-Z_]\w{5,17}@\w{2,10}\.(com|cn|net|com.cn)$/;
+			break;
+		case "post":
+			var r = /^[1-9]\d{5}$/;
+			break;
+		case "card":
+			var r = /^[1-9]\d{5}[12]\d{3}(0[1-9]|1[0-2])\d{5}[0-9X]$/;
+			break;
+		case "userphone":
+			var r = /^1[3-9]\d{9}$/;
+			break;
 	}
-	
-//密码输入判断
-	$("passId").onblur=function () {
-		// var r=/[a-zA-Z]\w{5,15}$/ig; 
-		var r=/^[0-9]*$/ig;		//6-16位字母,数字组合，且不能纯数字
-			if(r.test($("passId").value)){
-				$("content1").style.display="block";
-				$("content1").value="！请输入正确密码";
-			}else{
-				$("content1").style.display="none";
+	return r.test(str);
+}
+
+
+//注册验证
+var arr = ["username", "userpass", "userphone"];
+
+$(`#usernameId`).onblur = function () {
+	this.nextElementSibling.innerHTML = "";
+	if (check(this.value, "username")) {
+		afterCheckUser((has) => {
+			if (has) {
+				this.nextElementSibling.innerHTML = "用户名已经存在";
+			} else {
+				this.nextElementSibling.innerHTML = "√";
 			}
-	}
-
-//密码确认
-	$("qrPassId").onblur=function () {
-		// var r=/^[0-9]*$/ig;	//再次输入密码
-		if($("qrPassId").value==$("passId").value){
-			$("content1").style.display="none";
-		}else{
-			$("content1").style.display="block";
-			$("content1").value="！请输入相同的密码";
-		}
-	}
-	
-//手机号验证
-	$("phoneId").onblur=function () {
-		var r=/^\d{11}$/ig;//11位纯数字
-		if(r.test($("phoneId").value)){
-			$("content1").style.display="none";
-		}else{
-			$("content1").style.display="block";
-			$("content1").value="！请输入正确的手机号";
-		}
-	}
-//验证码验证
-
-	var num=parseInt(Math.random()*9000+1000);
-	$("box").innerHTML=num;
-	$("box").onclick=function (){
-		var num=parseInt(Math.random()*9000+1000);
-		$("box").innerHTML=num;
-	}
-	$("yzmId").onblur=function () {
-		if($("yzmId").value==$("box").innerHTML){
-			$("content1").style.display="none";
-		}else{
-			$("content1").style.display="block";
-			$("content1").value="！请输入正确的验证码";
-		}
-	}
-//提交检验
-
-	$("tiJiao").onclick=function (){
-		if(($("userId").value!="") && ($("passId").value!="") && ($("qrPassId").value!="") && ($("nameId").value!="") && ($("sfzId").value!="") && ($("phoneId").value!="") && ($("yzmId").value!="")&& ($("content1").style.display=="none")&&($("fuXuan").checked==true)){
-
-			alert("注册成功");
-		}else{
-			alert("请完善信息再提交");
-		}
+		}); //后端验证
+	} else {
+		this.nextElementSibling.innerHTML = "格式不正确";
 	}
 }
-	
+
+
+$(`#userpassId`).onblur = function () {
+	if (check(this.value, "userpass")) {
+		this.nextElementSibling.innerHTML = "√";
+	} else {
+		this.nextElementSibling.innerHTML = "×";
+	}
+}
+
+$(`#userphoneId`).onblur = function () {
+	if (check(this.value, "userphone")) {
+		this.nextElementSibling.innerHTML = "√";
+	} else {
+		this.nextElementSibling.innerHTML = "×";
+	}
+}
+
+$("#checkpass").onblur = function () {
+	if (checkpass()) {
+		this.nextElementSibling.innerHTML = "√";
+	} else {
+		this.nextElementSibling.innerHTML = "×";
+	}
+}
+
+//注册按钮绑定事件
+$("#btnReg").onclick = function () {
+	//1、先看前端验证是否全部通过
+	if (!frontCheck() || !checkpass()) {
+		alert("亲，请正确输入!");
+		return;
+	}
+	//2、用户名是否存在
+	if (isExistsUser) {
+		alert("亲，用户名存在，请重新思考");
+		return;
+	}
+	//2、后端注册
+	regSave();
+}
+
+
+//前端验证
+function frontCheck() {
+	for (let i in arr) {
+		if (!check($(`#${arr[i]}Id`).value, arr[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+//重复密码的验证
+function checkpass() {
+	return $("#userpassId").value == $("#checkpass").value;
+}
+
+//用户名是否存在
+var isExistsUser = false;
+//后端验证用户名是否存在
+function afterCheckUser(func) {
+	//1、创建对象
+	let xhr = new XMLHttpRequest();
+	//2、设置请求参数
+	let sendstr = "username=" + $("#usernameId").value;
+	xhr.open("get", "checkuser.php?" + sendstr, true);
+
+	//3、设置回调函数
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			isExistsUser = xhr.responseText == 1 ? true : false;
+			func(isExistsUser);
+		}
+	}
+	//4、发送请求
+	xhr.send();
+
+}
+
+//后端注册
+function regSave() {
+	//1、创建对象
+	let xhr = new XMLHttpRequest();
+
+	//2、设置请求参数
+	xhr.open("post", "regSave03.php", true);
+
+	//3、设置回调函数 
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			if (xhr.responseText == 1) {
+				$("#msg").innerHTML = "亲，恭喜您，注册成功！跳转到登录页面";
+				location.href = "indexs.html";
+			} else {
+				$("#msg").innerHTML = "亲，不好意思，注册失败！请重新输入内容";
+			}
+		}
+	}
+
+	//4、发送请求
+	//post请求，与get请求有两个地方不同：
+	//1)、设置请求头
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	//2)、把发送的数据作为send函数的参数
+
+	let sendstr = `username=${$("#usernameId").value}&userpass=${$("#userpassId").value}&userphone=${$("#userphoneId").value}`;
+	xhr.send(sendstr);
+}
+
+
+
+
+function $(str) {
+	if (str.charAt(0) == "#") {
+		return document.getElementById(str.substring(1));
+	} else if (str.charAt(0) == ".") {
+		return document.getElementsByClassName(str.substring(1));
+	} else {
+		return document.getElementsByTagName(str);
+	}
+}
